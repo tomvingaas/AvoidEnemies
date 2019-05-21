a869541591b56d40b2a2d45671751f61504958b1
Tom Andreas Krane Vingås <tomvingaas@outlook.com>
2019-05-21 18:28:21 +0200

    setting up game loop

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
@@ -32,7 +33,50 @@ public class Game extends Canvas implements Runnable {
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
 
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            while (delta >= 1){
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.GREEN);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
